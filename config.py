797109d626fe9068c416067f990800d7440c3d2c
aem2231 <mariojetullaji@gmail.com>
2023-12-05 20:27:47 +0000

    finished config hopefully

--- config.py
+++ config.py
@@ -1,6 +1,4 @@
-import string
 import os
-import json
 from cryptography.fernet import Fernet
 
 class Config():
@@ -9,7 +7,7 @@ class Config():
         self.passwordFilePath = "data/passwords"
         self.masterPasswordFilePath = "data/masterPassword.txt"
         self.passwords = "data/passwords.json"
-        
+
     def getKey(self):        
         if not os.path.exists("data"):
             os.makedirs("data")
@@ -19,24 +17,50 @@ class Config():
                 encryptionKey = f.read()
         else:
             encryptionKey = Fernet.generate_key()
-            with open(self.encryptionKeyFilePath, "w") as f:
+            with open(self.encryptionKeyFilePath, "wb") as f:
                 f.write(encryptionKey)
+        return encryptionKey
+    
+    def loadKey(self):
+        with open(self.encryptionKeyFilePath, "rb"):
+            return open(self.encryptionKeyFilePath, "rb").read()
                 
     def getMasterPassword(self):
-        if os.path.exists(self.masterPasswordFileFilePath):
+        if os.path.exists(self.masterPasswordFilePath):
             with open(self.masterPasswordFilePath) as f:
-                masterPassword = f.read()
+                encryptedPassword = f.read()
+                key = self.loadKey()
+                fernet = Fernet(key)
+                masterPassword = fernet.decrypt(encryptedPassword)
+                masterPassword = masterPassword.decode()
                 attempts = 0
                 passwordAttempt = ""
-                while passwordAttempt!= masterPassword:
+                while passwordAttempt != masterPassword:
                     attempts+=1
                     if attempts < 5:
-                        attempt = str(input(f"[Attempt {attempt}/5]\nEnter your master Password: "))
+                        passwordAttempt = str(input(f"[Attempt {attempts}/5]\nEnter your master Password: "))
                     elif attempts == 5:
                         print("Final attempt!")
-                        attempt = str(input("Enter your master Password: "))
-                    if attempt == masterPassword:
+                        passwordAttempt = str(input("Enter your master Password: "))
+                    if passwordAttempt == masterPassword:
                         print("Master Password Correct!")
+        else:
+            with open(self.masterPasswordFilePath, "wb") as f:
+                masterPassword = str(input("Please choose a master Password: "))
+                key = self.loadKey()
+                masterPassword = masterPassword.encode()
+                fernet = Fernet(key)
+                encryptedPassword = fernet.encrypt(masterPassword)
+                f.write(encryptedPassword)
+                print("Master Password Created!")
+                Config().getMasterPassword()
+
+def main():                        
+    config = Config()
+    config.getKey()
+    config.getMasterPassword()
+    
+main()
                     
   
                     
